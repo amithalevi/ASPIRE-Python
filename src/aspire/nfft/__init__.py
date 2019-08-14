f553b4abd6a8ab6008f7b2e18e3daa57b9bcbbac
Vineet Bansal <vineetbansal@gmail.com>
2019-08-14 17:31:27 -0400

    Removed support for nfft backends (pynfft/finufft)
    Just sticking to finufft while we debug the mac os x module loading issue.

--- src/aspire/nfft/__init__.py
+++ src/aspire/nfft/__init__.py
@@ -1,117 +1,83 @@
-import logging
-from collections import OrderedDict
 import numpy as np
-from aspire import config
-
-logger = logging.getLogger(__name__)
-
-# Cached Plan Class objects, indexed by backend string identifier, and ordered by preference (highest first)
-# The values are either Plan subclasses (for working backends), or None (for non-working backends)
-# Populated by 'check_backends()' when first needed.
-backends = None
-# Default preferred Plan subclass
-default_plan_class = None
-
-
-def check_backends(raise_errors=True):
-    """
-    Check all NFFT backends in package configuration
-    :param raise_errors: Whether to raise a RuntimeError if no backends detected.
-    :return: On return, the global names 'backends'/'default_plan_class' have been populated
-    """
-    global backends, default_plan_class
-
-    def _try_backend(backend):
-        """
-        This function tries out a particular NFFT backend by name.
-        :param backend: A string representing the NFFT backend we want to try. Currently one of:
-            'finufft'
-                The Python wrapper for the FlatIron Institute's FINUFFT library
-                https://github.com/flatironinstitute/finufft
-            'pynfft'
-                The Python wrapper for the Chemnitz NFFT library
-                https://www-user.tu-chemnitz.de/~potts/nfft/
-        :return: The proper Plan-subclass if the backend is expected to work or None otherwise.
-
-        It's important to keep these checks lightweight since all usable backend classes are cached on module load.
-        We only check for working imports here to keep things simple and lightweight.
-        If imports are working but the actual backend is not then we have bigger problems anyway.
-        """
-        logger.info(f"Trying NFFT backend {backend}")
-        plan_class = None
-        if backend == "finufft":
-            try:
-                from finufftpy import nufft1d1
-                from aspire.nfft.finufft import FINufftPlan
-                plan_class = FINufftPlan
-            except ImportError:
-                pass
-
-        elif backend == "pynfft":
-            try:
-                from pynfft.nfft import NFFT
-                from aspire.nfft.pynfft import PyNfftPlan
-                plan_class = PyNfftPlan
-            except ImportError:
-                pass
-
-        if plan_class is None:
-            logger.info(f"NFFT backend {backend} not usable")
-        else:
-            logger.info(f"NFFT backend {backend} usable")
-            return plan_class
-
-    backends = OrderedDict((k, _try_backend(k)) for k in config.nfft.backends)
-    try:
-        default_backend = next(k for k, v in backends.items() if v is not None)
-        logger.error(f'Selected NFFT backend = {default_backend}')
-        default_plan_class = backends[default_backend]
-    except StopIteration:
-        msg = "No usable NFFT backend detected."
-        logger.error(msg)
-        default_plan_class = None
-        if raise_errors:
-            raise RuntimeError(msg) from None
-
-
-def all_backends():
-    """
-    Determine all available NFFT backends
-    :return: A list of strings representing available NFFT backends
-    """
-    global backends
-
-    if backends is None:
-        check_backends(raise_errors=False)
-    return [k for k, v in backends.items() if v is not None]
-
-
-def backend_available(backend):
-    """
-    Whether a particular NFFT backend is available
-    :param backend: String representing the NFFT backend, e.g. 'finufft' or 'pynfft'
-    :return: Boolean on whether the backend is available
-    """
-    return backend in all_backends()
+from aspire.utils import ensure
 
 
 class Plan:
-    # TODO: move common functionality up the hierarchy
-    def __new__(cls, *args, **kwargs):
-        if cls is Plan:
-            if 'backend' in kwargs:
-                if backend_available(kwargs['backend']):
-                    return super(Plan, cls).__new__(backends[kwargs['backend']])
-                else:
-                    raise RuntimeError('Requested backend unavailable')
-            else:
-                # If a Plan was constructed as a generic Plan(), use the default (best) Plan class
-                if default_plan_class is None:
-                    check_backends(raise_errors=True)
-                return super(Plan, cls).__new__(default_plan_class)
-        else:
-            # If a Plan-subclass was constructed directly, invoke default behavior
-            return super(Plan, cls).__new__(cls)
+
+    def __init__(self, sz, fourier_pts, epsilon=1e-15):
+        """
+        A plan for non-uniform FFT (3D)
+        :param sz: A tuple indicating the geometry of the signal
+        :param fourier_pts: The points in Fourier space where the Fourier transform is to be calculated,
+            arranged as a 3-by-K array. These need to be in the range [-pi, pi] in each dimension.
+        :param epsilon: The desired precision of the NUFFT
+        """
+        self.sz = sz
+        self.dim = len(sz)
+        # TODO: Things get messed up unless we ensure a 'C' ordering here - investigate why
+        self.fourier_pts = np.asarray(np.mod(fourier_pts + np.pi, 2 * np.pi) - np.pi, order='C')
+        self.num_pts = fourier_pts.shape[1]
+        self.epsilon = epsilon
+
+        # Get a handle on the appropriate 1d/2d/3d forward transform function in finufftpy
+        import finufftpy
+        self.transform_function = getattr(finufftpy, {1: 'nufft1d2', 2: 'nufft2d2', 3: 'nufft3d2'}[self.dim])
+        # Get a handle on the appropriate 1d/2d/3d adjoint function in finufftpy
+        self.adjoint_function = getattr(finufftpy, {1: 'nufft1d1', 2: 'nufft2d1', 3: 'nufft3d1'}[self.dim])
+
+    def transform(self, signal):
+        ensure(signal.shape == self.sz, f'Signal to be transformed must have shape {self.sz}')
+
+        epsilon = max(self.epsilon, np.finfo(signal.dtype).eps)
+
+        # Forward transform functions in finufftpy have signatures of the form:
+        # (x, y, z, c, isign, eps, f, ...)
+        # (x, y     c, isign, eps, f, ...)
+        # (x,       c, isign, eps, f, ...)
+        # Where f is a Fortran-order ndarray of the appropriate dimensions
+        # We form these function signatures here by tuple-unpacking
+
+        result = np.zeros(self.num_pts).astype('complex128')
+
+        result_code = self.transform_function(
+            *self.fourier_pts,
+            result,
+            -1,
+            epsilon,
+            signal
+        )
+
+        if result_code != 0:
+            raise RuntimeError(f'FINufft transform failed. Result code {result_code}')
+
+        return result
+
+    def adjoint(self, signal):
+
+        epsilon = max(self.epsilon, np.finfo(signal.dtype).eps)
+
+        # Adjoint functions in finufftpy have signatures of the form:
+        # (x, y, z, c, isign, eps, ms, mt, mu, f, ...)
+        # (x, y     c, isign, eps, ms, mt      f, ...)
+        # (x,       c, isign, eps, ms,         f, ...)
+        # Where f is a Fortran-order ndarray of the appropriate dimensions
+        # We form these function signatures here by tuple-unpacking
+
+        # Note: Important to have order='F' here!
+        result = np.zeros(self.sz, order='F').astype('complex128')
+
+        result_code = self.adjoint_function(
+            *self.fourier_pts,
+            signal,
+            1,
+            epsilon,
+            *self.sz,
+            result
+        )
+        if result_code != 0:
+            raise RuntimeError(f'FINufft adjoint failed. Result code {result_code}')
+
+        return result
 
 
 def anufft3(vol_f, fourier_pts, sz, real=False):
--- tests/test_nfft.py
+++ tests/test_nfft.py
@@ -1,8 +1,8 @@
 import numpy as np
-from unittest import TestCase, skipUnless
+from unittest import TestCase
 from unittest.case import SkipTest
 
-from aspire.nfft import Plan, backend_available
+from aspire.nfft import Plan
 
 import os.path
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'saved_test_data')
@@ -15,9 +15,7 @@ class SimTestCase(TestCase):
     def tearDown(self):
         pass
 
-    def testTransform1(self):
-        if not backend_available('finufft'):
-            raise SkipTest
+    def testTransform(self):
 
         vol = np.load(os.path.join(DATA_DIR, 'nfft_volume.npy'))
         fourier_pts = np.array([
@@ -26,26 +24,7 @@ class SimTestCase(TestCase):
             [-3.93035749861843, -3.36417300942290, -2.79798852022738, -2.23180403103185]
         ])
 
-        plan = Plan(vol.shape, fourier_pts, backend='finufft')
-        result = plan.transform(vol)
-
-        self.assertTrue(np.allclose(
-            result,
-            [-0.05646675 + 1.503746j, 1.677600 + 0.6610926j, 0.9124417 - 0.7394574j, -0.9136836 - 0.5491410j]
-        ))
-
-    def testTransform2(self):
-        if not backend_available('pynfft'):
-            raise SkipTest
-
-        vol = np.load(os.path.join(DATA_DIR, 'nfft_volume.npy'))
-        fourier_pts = np.array([
-            [ 0.88952655922411,  0.35922344760724, -0.17107966400962, -0.70138277562649],
-            [ 1.87089316522016,  1.99362869011803,  2.11636421501590,  2.23909973991377],
-            [-3.93035749861843, -3.36417300942290, -2.79798852022738, -2.23180403103185]
-        ])
-
-        plan = Plan(vol.shape, fourier_pts, backend='pynfft')
+        plan = Plan(vol.shape, fourier_pts)
         result = plan.transform(vol)
 
         self.assertTrue(np.allclose(
